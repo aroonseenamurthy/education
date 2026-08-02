1a0c8192744064d0e5204e1ed67aa7bfdacfc4ed
Arun Murthy <arunsrinimurthy@gmail.com>
2026-08-01 20:13:55 -0400

    Initial Commit

---
+++ education/educationApp.swift
@@ -0,0 +1,21 @@
+//
+//  educationApp.swift
+//  education
+//
+//  Created by Programmer on 8/1/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct educationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
